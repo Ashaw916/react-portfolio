4f552d3b40638014eb8f3ddb84f37f1ac4bcc737
Aaron Young <aaron@auctionmethod.com>
2020-09-29 22:04:57 -0700

    fixing errors

---
+++ client/precache-manifest.48f96e0c274dd0c0d9db1169a13381fe.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132dba3ad1862ea39c83842a509faaac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20cc2bfceb1dfba11090",
+    "url": "/static/css/main.bf592883.chunk.css"
+  },
+  {
+    "revision": "f1f30cfa245ab84f6131",
+    "url": "/static/js/2.871b86d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.871b86d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20cc2bfceb1dfba11090",
+    "url": "/static/js/main.752abbac.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "71837401e9bf10020708e4716d4585fd",
+    "url": "/static/media/css.71837401.png"
+  },
+  {
+    "revision": "0f6642bd3c12c7541d506137884e2e79",
+    "url": "/static/media/firstPortfolio.0f6642bd.png"
+  },
+  {
+    "revision": "fb1e7f9db2540c3194a9179094a925e2",
+    "url": "/static/media/html.fb1e7f9d.png"
+  },
+  {
+    "revision": "a58728919c10c74e25a9b656c478f730",
+    "url": "/static/media/jquery.a5872891.png"
+  },
+  {
+    "revision": "b54fc1c9e4f229859a1b00205f5d14c0",
+    "url": "/static/media/js.b54fc1c9.png"
+  },
+  {
+    "revision": "d44d81a414fafd34191c8f452d01dd9a",
+    "url": "/static/media/mern.d44d81a4.png"
+  },
+  {
+    "revision": "34a00324b25d3eec16260ec0b8658158",
+    "url": "/static/media/mongo.34a00324.png"
+  },
+  {
+    "revision": "528234aa3ae3f5f644c63921cab09659",
+    "url": "/static/media/nodejs.528234aa.png"
+  },
+  {
+    "revision": "f9e3de8543b36f0a8e78dec84d65a01f",
+    "url": "/static/media/organize.f9e3de85.png"
+  },
+  {
+    "revision": "64e4547312306251d7fb2832f13bf817",
+    "url": "/static/media/organizev1.64e45473.png"
+  },
+  {
+    "revision": "24e0f6c062341fb59be5071196df2f79",
+    "url": "/static/media/profile_picture.24e0f6c0.jpg"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/static/media/react.04557c1b.png"
+  },
+  {
+    "revision": "ca5b9d80af2c6ff6731807dfc8a93701",
+    "url": "/static/media/restApi.ca5b9d80.png"
+  },
+  {
+    "revision": "21812f6d8d4844da17453b80216a4445",
+    "url": "/static/media/sql.21812f6d.png"
+  },
+  {
+    "revision": "087c50765b34761fe23b2b12972a1da3",
+    "url": "/static/media/trailLocator.087c5076.png"
+  },
+  {
+    "revision": "9364ffbb3a8c564f9085026e695bdbd3",
+    "url": "/static/media/weatherDashboard.9364ffbb.png"
+  },
+  {
+    "revision": "c0e89c5f2212647d214aaf98acbd9116",
+    "url": "/static/media/workoutTracker.c0e89c5f.png"
+  }
+]);
